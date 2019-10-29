328b7bca627780e76f5e4566858ecd2a8808e5f7
ElliotHarding <elliot.one.harding@gmail.com>
2019-10-29 17:18:41 +0000

    progress

--- Code/mybot.py
+++ Code/mybot.py
@@ -3,32 +3,28 @@ import aiml
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+import csv
 
 # Create a Kernel object. No string encoding (all I/O is unicode)
 kern = aiml.Kernel()
 kern.setTextEncoding(None)
 kern.bootstrap(learnFiles="mybot-basic.xml")
 
-# Welcome user
-print("Welcome")
+#Get questions & answers
+questionAnswerPairs = list(csv.reader(open('chatbot-qa-pairs.csv', 'r')))
+questions = [row[0] for row in questionAnswerPairs]
+answers = [row[1] for row in questionAnswerPairs]
 
+#Other global vars
 stopWords = ["the","is","an","a","at","and"]
 
-texts = ["Trees and dogs are main characters in this story",
-        "This is about airplanes and airlines",
-        "This is about dogs and houses too, but also about trees",
-        "Trees and dogs are main characters in this story",
-        "This story is about batman and superman fighting each other", 
-        "Nothing better than another story talking about airplanes, airlines and birds",
-        "Superman defeats batman in the last round"]
-
-def GetMostSimilar(string):
-    array = [string] + texts
+def GetMostSimilar(string, anArray):
+    array = [string] + anArray
     tfidf = TfidfVectorizer(stop_words=stopWords).fit_transform(array)
     similarityArray = cosine_similarity(tfidf[0:1], tfidf)
     similarityArray = np.delete(similarityArray, 0)
-    return texts[similarityArray.argmax()]
+    return anArray[similarityArray.argmax()]
 
-print(GetMostSimilar("This is dogs and houses too, but also about trees"))
+print(GetMostSimilar("This is dogs and houses too, but also about trees", questions))
 
 
