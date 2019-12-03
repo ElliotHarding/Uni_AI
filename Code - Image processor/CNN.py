652f2506e8123c92e3c4faf8532fc63bede5621c
Elliot <elliot.one.harding@gmail.com>
2019-12-03 02:43:36 +0000

    Coding additions
    Finished rough bot
    
    Changes
    
    Changes
    
    About to change dense 512
    
    Memes!
    
    CNN_space
    
    Changes
    
    changes

--- Code - Image processor/CNN.py
+++ Code - Image processor/CNN.py
@@ -7,16 +7,84 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-batch_size = 32
-epochs = 100
+#activate tf_gpu
+
+batch_size = 12 # amount of images processed at once 8
+epochs = 400
 IMG_HEIGHT = 150
 IMG_WIDTH = 150
 
-data_dir="aData\\"
+data_dir = "C:\\aData"
+classes = pd.read_csv('aData-names.csv')
 
-classes = pd.read_csv("aData-names.csv")
-class_names = classes.ClassName.tolist()
+data_dataGen = ImageDataGenerator(rescale=1./255, validation_split=0.2, horizontal_flip=True) # Generator for our validation data
+
+train_generator = data_dataGen.flow_from_directory(
+    data_dir,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=batch_size,
+    class_mode='categorical',
+    subset='training') # set as training data
+
+validation_generator = data_dataGen.flow_from_directory(
+    data_dir,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=batch_size,
+    class_mode='categorical',
+    subset='validation') # set as validation data
+
+sample_training_images, _ = next(train_generator)
 
 
+model = Sequential([
+    Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH , 3)),
+    MaxPooling2D(),
+    Dropout(0.2),
+    Conv2D(32, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Conv2D(64, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
+    Dropout(0.2),
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dense(120, activation='sigmoid')
+])
 
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
+#Output summary of model
+model.summary()
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch= 6108 // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps= 2036 // batch_size
+)
+
+model.save('C:\\dog-cnn.h5')
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
---
+++ Code - Image processor/CNN_space.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+import os
+import keras
+import matplotlib.pyplot as plt
+from keras.layers import Dense,GlobalAveragePooling2D
+from keras.applications import MobileNet
+from keras.preprocessing import image
+from keras.applications.mobilenet import preprocess_input
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+from keras.optimizers import Adam
+
+
+# In[2]:
+base_model=MobileNet(weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
+
+x=base_model.output
+x=GlobalAveragePooling2D()(x)
+x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
+x=Dense(1024,activation='relu')(x) #dense layer 2
+x=Dense(512,activation='relu')(x) #dense layer 3
+preds=Dense(120,activation='softmax')(x) #final layer with softmax activation
+
+
+# In[3]:
+model=Model(inputs=base_model.input,outputs=preds)
+#specify the inputs
+#specify the outputs
+#now a model has been created based on our architecture
+
+
+# In[4]:
+for layer in model.layers[:20]:
+    layer.trainable=False
+for layer in model.layers[20:]:
+    layer.trainable=True
+
+
+# In[5]:
+data_dir = "C:\\aData"
+image_size=244
+batch_size=32
+
+data_dataGen = ImageDataGenerator(preprocessing_function=preprocess_input, validation_split=0.2, horizontal_flip=True) # Generator for our validation data
+
+train_generator = data_dataGen.flow_from_directory(
+    data_dir,
+    target_size=(image_size, image_size),
+    batch_size=batch_size,
+    color_mode='rgb',
+    class_mode='categorical',
+    subset='training',
+    shuffle=True) # set as training data
+
+validation_generator = data_dataGen.flow_from_directory(
+    data_dir,
+    target_size=(image_size, image_size),
+    batch_size=batch_size,
+    color_mode='rgb',
+    class_mode='categorical',
+    subset='validation',
+    shuffle=True) # set as validation data
+
+
+# In[33]:
+model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
+# Adam optimizer
+# loss function will be categorical cross entropy
+# evaluation metric will be accuracy
+
+model.fit_generator(generator=train_generator,
+                    validation_data=validation_generator,
+                   steps_per_epoch=batch_size,
+                   epochs=5)
+
+model.save('C:\\dog-cnn.h5')
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
