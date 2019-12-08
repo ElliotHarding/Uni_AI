4b37729feefc8a6d8e0e1ec106f6c049e30ddf97
Elliot <elliot.one.harding@gmail.com>
2019-12-08 15:07:56 +0000

    Changed up traning model

--- Code - Image processor/CNN_space.py
+++ Code - Image processor/CNN_space.py
@@ -1,50 +1,54 @@
+#%tensorflow_version 2.4
+
+#Imports
 import pandas as pd
 import numpy as np
 import os
 import keras
 import matplotlib.pyplot as plt
-from keras.layers import Dense,GlobalAveragePooling2D
-from keras.applications import MobileNet
-from keras.preprocessing import image
-from keras.applications.mobilenet import preprocess_input
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Model
-from keras.optimizers import Adam
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, BatchNormalization
+from tensorflow.keras.layers import Dense,GlobalAveragePooling2D
+from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet import preprocess_input
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
 
 
-# In[2]:
+#Settings
+image_size=244
+batch_size=32
+epochs=75
+
+
+#Get pre-traied model & apply layers
 base_model=MobileNet(weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
 
 x=base_model.output
 x=GlobalAveragePooling2D()(x)
-x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
-x=Dense(1024,activation='relu')(x) #dense layer 2
-x=Dense(512,activation='relu')(x) #dense layer 3
-preds=Dense(120,activation='softmax')(x) #final layer with softmax activation
+x=Dense(1024,activation='relu')(x) #add dense layers so that model can learn more complex functions and classify for better results.
+x=Dense(1024,activation='relu')(x)
+x=Dense(512,activation='relu')(x) 
+preds=Dense(120,activation='softmax')(x)#final layer with softmax activation
 
-
-# In[3]:
 model=Model(inputs=base_model.input,outputs=preds)
-#specify the inputs
-#specify the outputs
-#now a model has been created based on our architecture
 
-
-# In[4]:
 for layer in model.layers[:20]:
     layer.trainable=False
 for layer in model.layers[20:]:
     layer.trainable=True
 
 
-# In[5]:
-data_dir = "C:\\aData"
-image_size=244
-batch_size=32
-epochs=10
+
+# Prep data
+data_dir = "C://aData"
+
 data_dataGen = ImageDataGenerator(preprocessing_function=preprocess_input, validation_split=0.2, horizontal_flip=True) # Generator for our validation data
 
-train_generator = data_dataGen.flow_from_directory(
+train_data = data_dataGen.flow_from_directory(
     data_dir,
     target_size=(image_size, image_size),
     batch_size=batch_size,
@@ -53,34 +57,37 @@ train_generator = data_dataGen.flow_from_directory(
     subset='training',
     shuffle=True) # set as training data
 
-validation_generator = data_dataGen.flow_from_directory(
+validation_data = data_dataGen.flow_from_directory(
     data_dir,
     target_size=(image_size, image_size),
     batch_size=batch_size,
     color_mode='rgb',
     class_mode='categorical',
-    subset='validation',
-    shuffle=True) # set as validation data
+    subset='validation') # set as validation data
 
 
-# In[33]:
+#Compile model
 model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
-# Adam optimizer
-# loss function will be categorical cross entropy
-# evaluation metric will be accuracy
 
 #keras.callbacks.EarlyStopping(monitor='val_loss',
                               #min_delta=0,
                               #patience=2,
                               #verbose=0, mode='auto')
 
-history = model.fit_generator(generator=train_generator,
-                    validation_data=validation_generator,
-                   steps_per_epoch=batch_size,
+#Run on GPU
+with tf.device("/gpu:0"):
+  history = model.fit_generator(generator=train_data,
+                    validation_data=validation_data,
+                   steps_per_epoch=train_data.samples/batch_size,
                    epochs=epochs)
 
-model.save('E:\\dog-cnn.h5')
+#Save model
+model.save('E://dog-cnnn.h5')
 
+
+
+
+#Output results
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
